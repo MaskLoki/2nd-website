0941abf1018b86af7f1f785458b18b9528068628
MaskLoki <117012952+MaskLoki@users.noreply.github.com>
2022-10-31 20:43:11 +0800

    Add files via upload

---
+++ WEB.js
@@ -0,0 +1,31 @@
+document.getElementById("button3").addEventListener("click", function() {
+    document.body.style.backgroundImage = "url('ussr.gif')"
+    //hide the bttn
+    document.querySelector(".everything3").style.display = "none"
+
+    document.getElementById("USSR").play()
+})
+
+document.getElementById("button4").addEventListener("click", function() {
+    document.body.style.backgroundImage = "url('pinas.gif')"
+    //hide the bttn
+    document.querySelector(".everything3").style.display = "none"
+
+    document.getElementById("Pinoy").play()
+})
+
+document.getElementById("button5").addEventListener("click", function() {
+    document.body.style.backgroundImage = "url('hitler.gif')"
+    //hide the bttn
+    document.querySelector(".everything3").style.display = "none"
+
+    document.getElementById("erika").play()
+})
+
+document.getElementById("button6").addEventListener("click", function() {
+    document.body.style.backgroundImage = "url('cat.gif')"
+    //hide the bttn
+    document.querySelector(".everything3").style.display = "none"
+
+    document.getElementById("anime").play()
+})
